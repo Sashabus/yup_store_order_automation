1fb90bfb5ac478de9e2fe58e652ac474dcc6a83c
macbook <vsashabus4321@gmail.com>
2023-12-25 03:37:51 +0200

    update frontend, add checkbox, email, passord, discord username input fields, initial chechout button validation

--- static/js/script.js
+++ static/js/script.js
@@ -9,19 +9,39 @@ function updateTotalPrice() {
 }
 
 function createBasketItem(product) {
-    const itemDiv = document.createElement('div');
-    itemDiv.className = 'product-item';
-    itemDiv.innerHTML = `
-        <span>${product.product_name}</span>
-        <div>
+    const isVbucks = product.product_name.toLowerCase().includes('v-bucks');
+
+    let quantityControl;
+    if (isVbucks) {
+        quantityControl = `
             <button type="button" onclick="updateQuantity(${product.product_id}, -1)">-</button>
             <input type="number" class="quantity-input" id="quantity-${product.product_id}"
                    data-price="${product.product_price_uah}" value="0" min="0" oninput="updateTotalPrice()">
             <button type="button" onclick="updateQuantity(${product.product_id}, 1)">+</button>
-            Price: ${product.product_price_uah} UAH
+        `;
+    } else {
+        quantityControl = `
+            <input type="checkbox" class="quantity-input" id="quantity-${product.product_id}"
+                   data-price="${product.product_price_uah}" value="0" onchange="updateCheckboxPrice(this, ${product.product_id})">
+        `;
+    }
+
+    const itemDiv = document.createElement('div');
+    itemDiv.className = 'product-item';
+    itemDiv.innerHTML = `
+        <span class="product-name">${product.product_name}</span>
+        <div class="product-controls">
+            ${quantityControl}
+            <span class="product-price">Price: ${product.product_price_uah} UAH</span>
         </div>
     `;
     return itemDiv;
+}
+
+function updateCheckboxPrice(checkbox, productId) {
+    const quantityInput = document.getElementById(`quantity-${productId}`);
+    quantityInput.value = checkbox.checked ? '1' : '0';
+    updateTotalPrice();
 }
 
 function updateQuantity(productId, change) {
@@ -53,5 +73,25 @@ function fetchProductsAndUpdateBasket() {
             console.error('Error fetching products:', error);
         });
 }
+
+function validateCheckout() {
+    const email = document.getElementById('fortnite-email').value;
+    const password = document.getElementById('fortnite-password').value;
+    const discordUsername = document.getElementById('discord-username').value;
+    let atLeastOneProductSelected = false;
+
+    document.querySelectorAll('.quantity-input').forEach(input => {
+        if (input.type === 'checkbox' && input.checked) atLeastOneProductSelected = true;
+        if (input.type === 'number' && parseInt(input.value) > 0) atLeastOneProductSelected = true;
+    });
+
+    const isFormValid = email && password && discordUsername && atLeastOneProductSelected;
+    document.getElementById('checkout-button').disabled = !isFormValid;
+}
+
+// Call this function on input/checkbox change and after fetching the products
+document.querySelectorAll('.user-info input').forEach(input => {
+    input.addEventListener('input', validateCheckout);
+});
 
 document.addEventListener('DOMContentLoaded', fetchProductsAndUpdateBasket);
